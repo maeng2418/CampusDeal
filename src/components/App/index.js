76c596f14d73281c7124e183b44cf3027973db66
maeng2418 <maeng2418@github.com>
2020-01-15 15:57:23 +0900

    Create Groups

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Footer from '../Footer';
 import Navigation from '../Navigation';
 import Search from '../Search';
+import Group from '../Group';
 
 const App = (props) => [
   <Navigation/>,
   <Search/>,
+  <Group/>,
   <Footer/>
 ];
 
--- src/components/Group/index.js
+++ src/components/Group/index.js
@@ -1,24 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Group extends Component.React{
+const Icon = (props) => (
+    <div>
+        <a href={props.url}>
+            <div>{props.title}</div>
+            <img src={props.image} alt="#"></img>
+        </a>
+    </div>
+);
+
+class Group extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            group:[
-                {id:1, title: '법학', image: "../../images/law.png", url:"#"},
-                {id:2, title: '경영/경제', image: "../../images/economics.png", url:"#"},
-                {id:3, title: '인문', image: "../../images/humanities.png", url:"#"},
-                {id:4, title: '어문', image: "../../images/linguistics.png", url:"#"},
-                {id:5, title: '사회과학', image: "../../images/sociology.png", url:"#"},
-                {id:6, title: '자연과학', image: "../../images/science.png", url:"#"},
-                {id:7, title: '공학', image: "../../images/engineering.png", url:"#"},
-                {id:8, title: '사범', image: "../../images/education.png", url:"#"},
-                {id:9, title: '생활환경', image: "../../images/life.png", url:"#"},
-                {id:10, title: '예체능', image: "../../images/art.png", url:"#"},
-                {id:11, title: '농축산', image: "../../images/agriculture.png", url:"#"},
-                {id:12, title: '의약학', image: "../../images/medical.png", url:"#"},
+            groups:[
+                {id:1, title: '법학', image: require("../../images/law.png"), url:"#"},
+                {id:2, title: '경영/경제', image: require("../../images/economics.png"), url:"#"},
+                {id:3, title: '인문', image: require("../../images/humanities.png"), url:"#"},
+                {id:4, title: '어문', image: require("../../images/linguistics.png"), url:"#"},
+                {id:5, title: '사회과학', image: require("../../images/sociology.png"), url:"#"},
+                {id:6, title: '자연과학', image: require("../../images/science.png"), url:"#"},
+                {id:7, title: '공학', image: require("../../images/engineering.png"), url:"#"},
+                {id:8, title: '사범', image: require("../../images/education.png"), url:"#"},
+                {id:9, title: '생활환경', image: require("../../images/life.png"), url:"#"},
+                {id:10, title: '예체능', image: require("../../images/art.png"), url:"#"},
+                {id:11, title: '농축산', image: require("../../images/agriculture.png"), url:"#"},
+                {id:12, title: '의약학', image: require("../../images/medical.png"), url:"#"},
 
             ]
         }
     }
+
+    render(){
+        const {groups} = this.state;
+        return(
+            <div className="group">
+                {groups.map((group)=>
+                    <Icon key={group.id} title={group.title} image={group.image}/>)}
+            </div>
+        )
+    }
 }
+
+export default Group;

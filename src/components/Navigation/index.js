0561dbfb16d483d777d3622fa002cebd76e5e777
maeng2418 <maeng2418@github.com>
2020-01-15 18:27:05 +0900

    Apply reset-css

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import styles from "./styles.module.scss";
 
 const Navigation = (props) => (
-    <div class="navigation">
-        <div class="nav-wrapper container">
-            <div class="row">
-                <div class="col">
-                    <div class="link">
+    <div className={styles.navigation}>
+        <div className={styles.navWrapper}>
+            <div className={styles.row}>
+                <div className={styles.col}>
+                    <div className={styles.link}>
                         <img src ={require("../../images/logo.png")} class="logo" alt="Logo"/>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="menu-bar">
-                        <ul class="menu">
-                            <li class="link">로그인</li>
-                            <li class="link">회원가입</li>
-                            <li class="link">장바구니</li>
-                            <li class="link">고객센터</li>
+                <div className={styles.col}>
+                    <div className={styles.menuBar}>
+                        <ul className={styles.menu}>
+                            <li className={styles.link}>로그인</li>
+                            <li className={styles.link}>회원가입</li>
+                            <li className={styles.link}>장바구니</li>
+                            <li className={styles.link}>고객센터</li>
                         </ul>
                     </div>
                 </div>

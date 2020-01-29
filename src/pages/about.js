864effe4eac4f061e85892cba8386041334527e4
maeng2418 <maeng2418@github.com>
2020-01-29 11:04:09 +0900

    Learning Routing with Query and Param

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Footer from 'components/Footer';
 import Navigation from 'components/Navigation';
+import queryString from 'query-string';
 
-const About = (props) => [
+const About = ({location, match}) => {
+  const query = queryString.parse(location.search);
+  console.log(query);
+  const detail = query.detail === 'true';
+
+  return [
     <Navigation/>,
-    <div>Hello</div>,
+    <div>
+      Hello {match.params.name} <br/>
+      {detail && 'detail: blahblah'}
+    </div>,
     <Footer/>
-  ];
+  ]
+
+};
 
 export default About;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Home, About, TestPage } from 'pages';
 
 
@@ -7,8 +7,13 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
+                <Route exact path="/" component={Home}/> {/*exact는 주어진 경로와 정확히 맞아 떨어져야함. 없을시, about이나 test가 합쳐짐*/}
+                
+                <Switch> {/*exact사용안해도 됨. 단, 순서 맞추어 주어야함. 위에서부터 검사해서 일치하면 리턴*/}
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
+                
                 <Route path="/test" component={TestPage}/>
             </div>
         );

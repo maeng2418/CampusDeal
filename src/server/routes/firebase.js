1a773269d5be58369c152119d43e1b0a2ddbcc25
maeng2418 <maeng2418@github.com>
2020-02-28 01:25:06 +0900

    GET and POST

---
+++ src/server/routes/firebase.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../config/serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://campus-deal-6637b.firebaseio.com"
+});   
+
+router.post('/api/login', (req, res, next) => {
+    admin.auth().getUserByEmail(req.body.email).then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully fetched user data:', userRecord.toJSON());
+  })
+  .catch(function(error) {
+   console.log('Error fetching user data:', error);
+  });
+});
+
+module.exports = router;
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,10 +1,34 @@
-var express = require('express');
-var router = express.Router();
-const os = require("os");
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get("/test", function(req, res, next){
-  res.send({ username: os.userInfo().username });
-  });
+/* GET home page. */
+router.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '나동빈',
+      'birthday': '960508',
+      'gender': '남자',
+      'job': '프로그래머'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이순신',
+      'birthday': '961127',
+      'gender': '남자',
+      'job': '디자이너'
+    }
+  ]);
+});
 
 module.exports = router;
